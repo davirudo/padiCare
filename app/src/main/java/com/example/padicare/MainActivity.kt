4afc643a1d02fd3cbc26cee1d5b758e9ae828036
David Diamanta <dantadiamanta@gmail.com>
2024-12-15 16:04:01 +0700

    testing out realtime code

--- app/src/main/java/com/example/padicare/MainActivity.kt
+++ app/src/main/java/com/example/padicare/MainActivity.kt
@@ -4,133 +4,113 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.Paint
 import android.graphics.SurfaceTexture
 import android.hardware.camera2.CameraCaptureSession
 import android.hardware.camera2.CameraDevice
 import android.hardware.camera2.CameraManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.HandlerThread
 import android.view.Surface
 import android.view.TextureView
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.example.padicare.ml.Testpadi
+import org.tensorflow.lite.support.image.ImageProcessor
 import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.ResizeOp
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var bitmap: Bitmap
-    lateinit var imageView: ImageView
-    lateinit var cameraDevice: CameraDevice
-    lateinit var handler: Handler
-    lateinit var cameraManager: CameraManager
-    lateinit var textureView: TextureView
+    private lateinit var paint: Paint
+    private lateinit var imageProcessor: ImageProcessor
+    private lateinit var bitmap: Bitmap
+    private lateinit var imageView: ImageView
+    private lateinit var cameraDevice: CameraDevice
+    private lateinit var handler: Handler
+    private lateinit var cameraManager: CameraManager
+    private lateinit var textureView: TextureView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        getPermission()
+        requestCameraPermission()
 
-        val handlerThread = HandlerThread("videoThread")
-        handlerThread.start()
+        imageProcessor = ImageProcessor.Builder().add(ResizeOp(300, 300, ResizeOp.ResizeMethod.BILINEAR)).build()
+
+        val handlerThread = HandlerThread("VideoThread").apply { start() }
         handler = Handler(handlerThread.looper)
 
         imageView = findViewById(R.id.imageView)
-
         textureView = findViewById(R.id.textureView)
-        textureView.surfaceTextureListener = object: TextureView.SurfaceTextureListener {
-            override fun onSurfaceTextureAvailable(p0: SurfaceTexture, p1: Int, p2: Int) {
-                openCamera()
-            }
-
-            override fun onSurfaceTextureSizeChanged(p0: SurfaceTexture, p1: Int, p2: Int) {
-
-            }
-
-            override fun onSurfaceTextureDestroyed(p0: SurfaceTexture): Boolean {
-                return false
-            }
-
-            override fun onSurfaceTextureUpdated(p0: SurfaceTexture) {
-                bitmap = textureView.bitmap!!
-
-                val model = Testpadi.newInstance(this)
-
-                val image = TensorImage.fromBitmap(bitmap)
-
-                val outputs = model.process(image)
-                val output = outputs.outputAsCategoryList
-
-                model.close()
-            }
-
-        }
+        textureView.surfaceTextureListener = TextureListener()
 
         cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
-
-
     }
 
     @SuppressLint("MissingPermission")
-    fun openCamera(){
-        cameraManager.openCamera(cameraManager.cameraIdList[0], object : CameraDevice.StateCallback(){
-            override fun onOpened(p0: CameraDevice) {
-                cameraDevice = p0
+    private fun openCamera() {
+        cameraManager.openCamera(cameraManager.cameraIdList[0], object : CameraDevice.StateCallback() {
+            override fun onOpened(device: CameraDevice) {
+                cameraDevice = device
+                val surfaceTexture = textureView.surfaceTexture
+                val surface = Surface(surfaceTexture)
 
-                var surfaceTexture = textureView.surfaceTexture
-                var surface = Surface(surfaceTexture)
-                
-                var captureRequest = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
-                captureRequest.addTarget(surface)
-                
+                val captureRequest = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW).apply {
+                    addTarget(surface)
+                }
+
                 cameraDevice.createCaptureSession(listOf(surface), object : CameraCaptureSession.StateCallback() {
-                    override fun onConfigured(p0: CameraCaptureSession) {
-                        p0.setRepeatingRequest(captureRequest.build(), null, null)
+                    override fun onConfigured(session: CameraCaptureSession) {
+                        session.setRepeatingRequest(captureRequest.build(), null, handler)
                     }
 
-                    override fun onConfigureFailed(p0: CameraCaptureSession) {
-                        TODO("Not yet implemented")
-                    }
-
-
-                })
+                    override fun onConfigureFailed(session: CameraCaptureSession) {}
+                }, handler)
             }
 
-            override fun onDisconnected(p0: CameraDevice) {
-
-            }
-
-            override fun onError(p0: CameraDevice, p1: Int) {
-
-            }
-
+            override fun onDisconnected(device: CameraDevice) {}
+            override fun onError(device: CameraDevice, error: Int) {}
         }, handler)
     }
 
-
-
-    fun getPermission(){
-        if (
-            ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 101)
-            }
+    private fun requestCameraPermission() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 101)
+        }
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if(
-            grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-            getPermission()
+        if (grantResults.isNotEmpty() && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+            requestCameraPermission()
+        }
+    }
+
+    private inner class TextureListener : TextureView.SurfaceTextureListener {
+        override fun onSurfaceTextureAvailable(surface: SurfaceTexture, width: Int, height: Int) {
+            openCamera()
+        }
+
+        override fun onSurfaceTextureSizeChanged(surface: SurfaceTexture, width: Int, height: Int) {}
+
+        override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean {
+            return false
+        }
+
+        override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {
+            bitmap = textureView.bitmap ?: return
+
+            val model = Testpadi.newInstance(this@MainActivity)
+            val tensorImage = TensorImage.fromBitmap(bitmap)
+            val outputs = model.process(tensorImage)
+            val detectionResults = outputs.outputAsCategoryList
+
+            // Handle the detection results
+            model.close()
         }
     }
 }
 
-private fun CameraDevice.createCaptureSession(listOf: List<Surface>, stateCallback: CameraCaptureSession.StateCallback) {
-
-}
